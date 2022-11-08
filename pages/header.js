7bf49ee430bb31d48ed0b18f62e2f0183bee2184
MustafaSKaya <mustafakya88@hotmail.com>
2022-11-08 18:22:00 -0500

    typed.js is integrated with the header section

--- pages/header.js
+++ pages/header.js
@@ -1,4 +1,27 @@
+import Typed from "typed.js";
+import { useEffect, useRef } from "react";
+
 export default function header() {
+
+    const element = useRef(null);
+
+    useEffect(() => {
+        const typed = new Typed(element.current, {
+          strings: ["Adam Adamson","A Full-Stack Web Developer","A Software Engineer","Your best employee candidate"], // Strings to display
+          // Speed settings, try diffrent values untill you get good results
+          startDelay: 300,
+          typeSpeed: 100,
+          backSpeed: 100,
+          backDelay: 100,
+          loop: true,
+          loopCount: Infinity
+        });
+
+        return () => {
+            typed.destroy();
+          };
+    }, []);
+
     return (
         <header id="home">
             <div className="container-fluid header-content">
@@ -6,8 +29,8 @@ export default function header() {
                     <div className="col text-center">
                         <div className="content-box">
                             <h4>Welcome</h4>
-                            <h1><span className="element"></span></h1>
-                            <p><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia rem quas autem porro. Natus sed quidem vitae est id aut deleniti atque amet, adipisci labore, eligendi, consectetur suscipit? Architecto, molestiae.</span><span>Mollitia fugit quidem quod quae maxime et aperiam itaque ad minus voluptates corporis fuga, quis facilis explicabo totam? Perferendis eaque ducimus repellendus harum voluptas optio veniam. Voluptatibus velit ducimus nemo.</span><span>Ut inventore similique ad nobis deleniti fugit hic dolorem molestias expedita corporis, voluptatibus consectetur! In quos, ab eveniet commodi similique architecto facilis delectus. Odit, exercitationem expedita molestias sunt ipsum id!</span></p>
+                            <h1><span ref={element} className="element"></span></h1>
+                            <p><span></span></p>
                             <a href="#" className="btn">Download Resume</a>
                         </div>
                     </div>
--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,14 @@ import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 import Navigation from './nav.js';
 import Header from './header.js';
+import About from './about.js'
 
 export default function Home() {
   return (
     <div>
       <Navigation/>
       <Header/>
+      <About/>
     </div>
   )
 }
